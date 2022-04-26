6148c7f23520bbb07e844dee005852f725eccbd9
soduma <soduma@naver.com>
2022-04-26 14:53:12 +0900

    Initial Commit

---
+++ PraticeCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PraticeCoreData
+//
+//  Created by 장기화 on 2022/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
